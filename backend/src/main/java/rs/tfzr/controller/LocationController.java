925e32733c6b494306c9b9927ed0febefec9c122
radmila-vukelja <emailzaseminarskitest123321>
2021-08-29 12:36:01 +0200

    [SFS#12] - dodata mogucnost ulogovanja

--- backend/src/main/java/rs/tfzr/controller/LocationController.java
+++ backend/src/main/java/rs/tfzr/controller/LocationController.java
@@ -3,6 +3,7 @@ package rs.tfzr.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import rs.tfzr.model.Location;
 import rs.tfzr.service.LocationService;
