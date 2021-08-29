8af892fc3b66a39f339753f2d4d6933f925d3fa7
lazar-todorovic <lazar.todorovskievich@gmail.com>
2021-08-29 14:14:18 +0200

    [SFS#4] - dodata funkcionalnost dodavanja lokacije

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LocationsComponent } from './components/locations/locations.component';
 import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   { path: '', component: RegisterComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'locations', component: LocationsComponent },
 ];
 
 @NgModule({
